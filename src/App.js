2876d2f1c746d2e2e1983368242c62bb0d6e97c2
tunglv <lvtungdn43@gmail.com>
2019-06-28 23:51:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './component/Navbar';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="wrapper">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
